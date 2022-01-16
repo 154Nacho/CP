7dc39633e3f372792473d4fb7465ae4dca9945ad
Pedro Barbosa <pedromiguelbarbosa05@gmail.com>
2022-01-16 01:14:26 +0000

    sequencial

--- bucketSort.c
+++ bucketSort.c
@@ -2,153 +2,118 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define NARRAY 15   // Array size
-#define NBUCKET 7  // Number of buckets
-#define INTERVAL 10  // Each bucket capacity
+// Array size nArray
+// Number of buckets nBucket
+// Each bucket capacity intervalo
 
-struct Node {
-  int data;
-  struct Node *next;
-};
+// Function to sort the elements of each bucket
 
-void BucketSort(int arr[]);
-struct Node *InsertionSort(struct Node *list);
-void print(int arr[]);
-void printBuckets(struct Node *list);
-int getBucketIndex(int value);
+void myBubbleSort(int arr[], int n) {
+    int i, j, temp;
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n-i-1; j++)
+        {
+            if( arr[j] > arr[j+1])
+            {
+                // swap the elements
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            } 
+        }
+    }
+}
+
+int getBucketIndex(int value,int intervalo) {
+  return value / intervalo;
+}
+
+void print(int ar[],int nARRAY) {
+  int i;
+  for (i = 0; i < nARRAY; ++i) {
+    printf("%d ", ar[i]);
+  }
+  printf("\n");
+}
 
 // Sorting function
-void BucketSort(int arr[]) {
+void BucketSort(int arr[],int nARRAY,int nBUCKET) {
+
+  int intervalo = nARRAY/nBUCKET;
+  int **n_Buckets = (int **)malloc(nBUCKET * sizeof(int *));
+  int *n_Buckets_MAX = (int *)malloc(nBUCKET * sizeof(int));
+  int *n_Buckets_ATUAL = (int *)malloc(nBUCKET * sizeof(int)); 
   int i, j;
-  struct Node **buckets;
 
-  // Create buckets and allocate memory size
-  buckets = (struct Node **)malloc(sizeof(struct Node *) * NBUCKET);
 
-  // Initialize empty buckets
-  for (i = 0; i < NBUCKET; ++i) {
-    buckets[i] = NULL;
+  for (i = 0; i < nBUCKET ; ++i)
+      n_Buckets[i] = (int *)malloc(intervalo * sizeof(int));
+      n_Buckets_MAX[i] = intervalo;
+      n_Buckets_ATUAL[i] = 0;
+      
+  for (i = 0; i < nARRAY; ++i) {
+    int posBucket = getBucketIndex(arr[i],intervalo);
+    int posAtual = n_Buckets_ATUAL[posBucket];
+    int bucketMax = n_Buckets_MAX[posBucket];
+    if (posAtual == bucketMax) {
+      int aumento = bucketMax + (0.2 * intervalo);
+      n_Buckets[posBucket] = realloc (n_Buckets[posBucket], sizeof(int) * (aumento));
+      n_Buckets_MAX[posBucket] = bucketMax + (0.2 * intervalo);
+    }
+    n_Buckets[posBucket][posAtual] = arr[i]; 
+    n_Buckets_ATUAL[posBucket]++;
   }
 
-  // Fill the buckets with respective elements
-  for (i = 0; i < NARRAY; ++i) {
-    struct Node *current;
-    int pos = getBucketIndex(arr[i]);
-    current = (struct Node *)malloc(sizeof(struct Node));
-    current->data = arr[i];
-    current->next = buckets[pos];
-    buckets[pos] = current;
-  }
-
-  // Print the buckets along with their elements
-  for (i = 0; i < NBUCKET; i++) {
-    printf("Bucket[%d]: ", i);
-    printBuckets(buckets[i]);
+  for (i = 0; i < nBUCKET; i++) {
+    for (j = 0; j < n_Buckets_ATUAL[i]; j++) {
+      printf("Bucket[%d] = %d\n", i, n_Buckets[i][j]);
+    }
     printf("\n");
   }
 
-  // Sort the elements of each bucket
-  for (i = 0; i < NBUCKET; ++i) {
-    buckets[i] = InsertionSort(buckets[i]);
+  for (i = 0; i < nBUCKET; i++) {
+    for (j = 0; j < n_Buckets_ATUAL[i]; j++) {
+      myBubbleSort(n_Buckets[i],n_Buckets_ATUAL[i]);
+    }
   }
 
   printf("-------------\n");
-  printf("Bucktets after sorting\n");
-  for (i = 0; i < NBUCKET; i++) {
-    printf("Bucket[%d]: ", i);
-    printBuckets(buckets[i]);
+  printf("Buckets after sorting\n");
+  for (i = 0; i < nBUCKET; i++) {
+    for (j = 0; j < n_Buckets_ATUAL[i]; j++) {
+      printf("Bucket[%d] = %d\n", i, n_Buckets[i][j]);
+    }
     printf("\n");
   }
 
-  // Put sorted elements on arr
-  for (j = 0, i = 0; i < NBUCKET; ++i) {
-    struct Node *node;
-    node = buckets[i];
-    while (node) {
-      arr[j++] = node->data;
-      node = node->next;
+  int k = 0;
+  for (i = 0; i < nBUCKET; i++) {
+    for (j = 0; j < n_Buckets_ATUAL[i]; j++) {
+      arr[k++] = n_Buckets[i][j];
     }
   }
 
   return;
 }
 
-// Function to sort the elements of each bucket
-struct Node *InsertionSort(struct Node *list) {
-  struct Node *k, *nodeList;
-  if (list == 0 || list->next == 0) {
-    return list;
-  }
 
-  nodeList = list;
-  k = list->next;
-  nodeList->next = 0;
-  while (k != 0) {
-    struct Node *ptr;
-    if (nodeList->data > k->data) {
-      struct Node *tmp;
-      tmp = k;
-      k = k->next;
-      tmp->next = nodeList;
-      nodeList = tmp;
-      continue;
-    }
-
-    for (ptr = nodeList; ptr->next != 0; ptr = ptr->next) {
-      if (ptr->next->data > k->data)
-        break;
-    }
-
-    if (ptr->next != 0) {
-      struct Node *tmp;
-      tmp = k;
-      k = k->next;
-      tmp->next = ptr->next;
-      ptr->next = tmp;
-      continue;
-    } else {
-      ptr->next = k;
-      k = k->next;
-      ptr->next->next = 0;
-      continue;
-    }
-  }
-  return nodeList;
-}
-
-int getBucketIndex(int value) {
-  return value / INTERVAL;
-}
-
-void print(int ar[]) {
-  int i;
-  for (i = 0; i < NARRAY; ++i) {
-    printf("%d ", ar[i]);
-  }
-  printf("\n");
-}
-
-// Print buckets
-void printBuckets(struct Node *list) {
-  struct Node *cur = list;
-  while (cur) {
-    printf("%d ", cur->data);
-    cur = cur->next;
-  }
-}
-
-// Driver code
-int main(void) {
-  int array[NARRAY] = {42, 32, 10, 33, 52, 22, 37, 63, 47, 51, 5, 36, 1, 67, 53};
+int main(int argc,char **argv) {
+  const int nARRAY = atoi(argv[1]);
+  const int nBUCKET = atoi(argv[2]);
+  int *array_Mega;
+  array_Mega = malloc(nARRAY*sizeof(int));
+  for (int i = 0; i < nARRAY; i++) array_Mega[i] = rand() % nARRAY;
 
   printf("Initial array: ");
-  print(array);
+  print(array_Mega,nARRAY);
   printf("-------------\n");
 
-  BucketSort(array);
+  BucketSort(array_Mega,nARRAY,nBUCKET);
   printf("-------------\n");
   printf("Sorted array: ");
-  print(array);
+  print(array_Mega,nARRAY);
   return 0;
 }
